1c7a89526390f582c907c31af4c46c9f6c73af18
Michael <rc@bellergy.com>
2018-06-05 19:27:28 +0800

    Add more regions selection

--- index.php
+++ index.php
@@ -7,83 +7,104 @@ $c = explode("\n",$c);
 
 if(!isset($_POST['submit'])) {
 ?>
-<!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-<!--
-    |                  |
-_ \ |  _` |  _ \ __ \  __|  _ \   __|
-__/ | (   |  __/ |   | |   (   |\__ \
-\___|_|\__, |\___|_|  _|\__|\___/ ____/
-    |___/
-    Elgentos Ecommerce Solutions
-        www.elgentos.nl
-    info@elgentos.nl / 050-7001515
+    <!doctype html>
+    <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+    <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+    <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+    <!--[if gt IE 8]><!-->
+    <html class="no-js" lang="en">
+    <!--<![endif]-->
 
-    Oh hi there! o/
+    <head>
+        <!--   
+		Website designed by http://designquest.com.hk/
+			  ___  ___ ___ ___ ___ _  _    ___  _   _ ___ ___ _____ 
+			 |   \| __/ __|_ _/ __| \| |  / _ \| | | | __/ __|_   _|
+			 | |) | _|\__ \| | (_ | .` | | (_) | |_| | _|\__ \ | |  
+			 |___/|___|___/___\___|_|\_|  \__\_\\___/|___|___/ |_|  
+		-->
 
--->
-<meta charset="utf-8">
-<title>Magento Table Rates Generator</title>
-<meta name="description" content="Generate your table rates (tablerates.csv) for Magento shipping!" />
-<meta name="keywords" content="magento, table, rates, tablerates, tablerates.csv, magento, shipping, costs, generator" />
-<meta name="author" content="Elgentos Ecommerce Solutions" />
-<meta name="viewport" content="width=device-width,initial-scale=1" />
-<!--[if ie]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="generator.css">
+        <meta charset="utf-8">
+        <title>Magento Table Rates Generator - Design Quest</title>
+        <meta name="description" content="Generate your table rates (tablerates.csv) for Magento shipping!"
+        />
+        <meta name="keywords" content="magento, table, rates, tablerates, tablerates.csv, magento, shipping, costs, generator"
+        />
+        <meta name="author" content="Elgentos Ecommerce Solutions" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <!--[if ie]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><![endif]-->
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="generator.css">
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="javascript.js"></script>
-<script type="text/javascript">
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script type="text/javascript" src="javascript.js"></script>
+    </head>
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-1292775-30']);
-_gaq.push(['_trackPageview']);
+    <body>
+        <h1>Magento Table Rates Generator</h1>
 
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+        <input type="button" name="button" onclick="checkCounties('asia')" value="Check all countries in Asia" />
+        <br />
 
-</script>
-</head>
+        <input type="button" name="button" onclick="checkCounties('na')" value="Check all countries in Northern America" />
+        <br />
 
-<body>
-<h1>Elgentos' Magento Table Rates Generator</h1>
-<ul>
-<li>- You can use this generator to generate the tablerates.csv file to import into your Magento installation.</li>
-<li>- You can check all the countries in the European Union at once, and set default "Shipping Costs" and "From &lt;condition&gt;" for the checked countries.</li>
-<li>- Click "+ Add row" to add an extra row to allow tier pricing for shipping costs.</li>
-<li>- Click "Generate tablesrates.csv" to download the generated CSV file.</li>
-<li>-
-<?php if(!isset($_GET['twoLetterCodes'])) { ?>
-For older Magento versions (<1.4), 2-letter country codes are needed. <input type="button" name="button" onclick="document.location.href='/tablerates/?twoLetterCodes'" value="Switch to 2-letter country codes" />
-<?php  } else { ?>
-For newer Magento versions (>1.4), 3-letter country codes are needed. <input type="button" name="button" onclick="document.location.href='/tablerates/'" value="Switch to 3-letter country codes" />
-<?php  } ?></li>
-</ul>
-<br />
-<input type="button" name="button" onclick="checkEU()" value="Check all countries in European Union" />
-<br />
-<input type="button" name="button" onclick="checkEuro()" value="Check all European countries" />
-<br />
-<br />
-Default price: <input type="text" value="12.50" id="defPrice" name="defPrice" /> <br /><input type="button" name="button" onclick="setDefPrice()" value="Set default Shipping Costs for checked countries" /><br /><br />
-Table rates condition: <select id="carriers_tablerate_condition_name" name="carriers_tablerate_condition_name" onchange="changeLabel()">
-    <option value="weight">Weight vs. Destination</option>
-    <option value="price" selected="selected">Price vs. Destination</option>
-    <option value="qty"># of Items vs. Destination</option>
-</select><br />
-Default From condition value: <input type="text" value="0" id="defSubtotal" name="defSubtotal" /> <br /><input type="button" name="button" onclick="setdefSubtotal()" value="Set default condition value for checked countries" /><br /><br />
-<form action="" method="post">
-<ul>
-<li><table><tr><th style="width:250px;">Country</th><th>Shipping Costs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span id="conditionLabel">From subtotal</span></th><th>&nbsp;</th></tr></table></li>
-<?php
+        <input type="button" name="button" onclick="checkCounties('americas')" value="Check all countries in Americas" />
+        <br />
+
+        <input type="button" name="button" onclick="checkCounties('europe')" value="Check all countries in Europe" />
+        <br />
+
+        <input type="button" name="button" onclick="checkCounties('oceania')" value="Check all countries in Oceania" />
+        <br />
+
+        <input type="button" name="button" onclick="checkCounties('africa')" value="Check all countries in Africa" />
+        <br />
+        <br />
+
+        <input type="button" name="button" onclick="reset()" value="Reset checks">
+        <br />
+
+        <input type="button" name="button" onclick="clearValues()" value="Clear values for selected countries">
+        <br />
+
+        <br /> Default price:
+        <input type="text" value="12.50" id="defPrice" name="defPrice"
+        />
+        <br />
+        <input type="button" name="button" onclick="setDefPrice()" value="Set default Shipping Costs for checked countries"
+        />
+        <br />
+        <br /> Table rates condition:
+        <select id="carriers_tablerate_condition_name" name="carriers_tablerate_condition_name"
+            onchange="changeLabel()">
+            <option value="weight">Weight vs. Destination</option>
+            <option value="price" selected="selected">Price vs. Destination</option>
+            <option value="qty"># of Items vs. Destination</option>
+        </select>
+        <br /> Default From condition value:
+        <input type="text" value="0" id="defSubtotal"
+            name="defSubtotal" />
+        <br />
+        <input type="button" name="button" onclick="setdefSubtotal()" value="Set default condition value for checked countries"
+        />
+        <br />
+        <br />
+        <form action="" method="post">
+            <ul>
+                <li>
+                    <table>
+                        <tr>
+                            <th style="width:250px;">Country</th>
+                            <th>Shipping Costs&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <span
+                                    id="conditionLabel">From subtotal</span>
+                            </th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </table>
+                </li>
+                <?php
 foreach($three as $part) {
     $parts3 = explode(" - ",$part);
     $threes[$parts3[0]] = $parts3[1];
@@ -97,26 +118,44 @@ foreach($c as $d) {
         $countryCode = $threes[$parts[0]];
     }
     ?>
-    <li>
-        <table>
-            <tr>
-                <td style="width: 250px;"><input type='checkbox' name='country_<?=$countryCode;?>[]' value='<?=$countryCode;?>' id='country_<?=$countryCode;?>' /> <label><?=$parts[1];?></label></td>
-                <td>&euro; <input type="text" name="price_<?=$countryCode;?>[]" />&nbsp;&nbsp;<span class="euroCondition">&euro;</span> <input type="text" name="from_<?=$countryCode;?>[]" /></td>
-                <td><a href="javascript:void()" class="addRow">+ Add row</a>
-            </tr>
-        </table>
-    </li>
-    <?php
+                    <li>
+                        <table>
+                            <tr>
+                                <td style="width: 250px;">
+                                    <input type='checkbox' name='country_<?=$countryCode;?>[]'
+                                        value='<?=$countryCode;?>' id='country_<?=$countryCode;?>'
+                                    />
+                                    <label>
+                                        <?=$parts[1];?>
+                                    </label>
+                                </td>
+                                <td>$
+                                    <input type="text" name="price_<?=$countryCode;?>[]"
+                                    />&nbsp;&nbsp;
+                                    <span class="euroCondition">$</span>
+                                    <input type="text" name="from_<?=$countryCode;?>[]"
+                                    />
+                                </td>
+                                <td>
+                                    <a href="javascript:void()" class="addRow">+ Add row</a>
+                            </tr>
+                        </table>
+                    </li>
+                    <?php
 }
 ?>
-</ul>
-<br />
-<input type="submit" name="submit" value="Generate tablerates.csv" />
-<small>&copy; 2012 <a href="http://elgentos.nl" title="Elgentos Ecommerce Solutions" alt="Elgentos Ecommerce Solutions">Elgentos Ecommerce Solutions</a></small>
-</form>
-</body>
-</html>
-<?php } else {
+            </ul>
+            <br />
+            <input type="submit" name="submit" value="Generate tablerates.csv" />
+            <small>&copy; 2012
+                <a href="http://elgentos.nl" title="Elgentos Ecommerce Solutions"
+                    alt="Elgentos Ecommerce Solutions">Elgentos Ecommerce Solutions</a>
+            </small>
+        </form>
+    </body>
+
+    </html>
+    <?php } else {
     header("Content-Type: text/plain");
     header("Content-type: application/force-download");
     header("Content-Transfer-Encoding: Binary");
--- javascript.js
+++ javascript.js
@@ -1,16 +1,42 @@
-function checkEU() {
-    if(document.location.href.indexOf("twoLetterCodes")>0) {
-        var EU = new Array("BE","BG","CY","DK","DE","EE","FI","FR","GR","HU","HR","IE","IT","LV","LT","LU","MT","NL","AT","PL","PT","RO","SK","SI","ES","CZ","GB","SE");
-    } else {
-        var EU = new Array("BEL","BGR","CYP","DNK","DEU","EST","FIN","FRA","GRC","HUN","HRV","IRL","ITA","LVA","LTU","LUX","MLT","NLD","AUT","POL","PRT","ROU","SVK","SVN","ESP","CZE","GBR","SWE");
+var asia = ["AFG", "ARM", "AZE", "BHR", "BGD", "BTN", "BRN", "KHM", "CHN", "CYP", "GEO", "HKG", "IND", "IDN", "IRN", "IRQ", "ISR", "JPN", "JOR", "KAZ", "PRK", "KOR", "KWT", "KGZ", "LAO", "LBN", "MAC", "MYS", "MDV", "MNG", "MMR", "NPL", "OMN", "PAK", "PSE", "PHL", "QAT", "SAU", "SGP", "LKA", "SYR", "TWN", "TJK", "THA", "TLS", "TUR", "TKM", "ARE", "UZB", "VNM", "YEM"];
+var americas = ["AIA", "ATG", "ARG", "ABW", "BHS", "BRB", "BLZ", "BMU", "BOL", "BES", "BVT", "BRA", "CAN", "CYM", "CHL", "COL", "CRI", "CUB", "CUW", "DMA", "DOM", "ECU", "SLV", "FLK", "GUF", "GRL", "GRD", "GLP", "GTM", "GUY", "HTI", "HND", "JAM", "MTQ", "MEX", "MSR", "NIC", "PAN", "PRY", "PER", "PRI", "BLM", "KNA", "LCA", "MAF", "SPM", "VCT", "SXM", "SGS", "SUR", "TTO", "TCA", "USA", "URY", "VEN", "VGB", "VIR"];
+var na = ["BMU", "CAN", "GRL", "SPM", "USA"];
+var europe = ["ALA", "ALB", "AND", "AUT", "BLR", "BEL", "BIH", "BGR", "HRV", "CZE", "DNK", "EST", "FRO", "FIN", "FRA", "DEU", "GIB", "GRC", "GGY", "VAT", "HUN", "ISL", "IRL", "IMN", "ITA", "JEY", "LVA", "LIE", "LTU", "LUX", "MKD", "MLT", "MDA", "MCO", "MNE", "NLD", "NOR", "POL", "PRT", "ROU", "RUS", "SMR", "SRB", "SVK", "SVN", "ESP", "SJM", "SWE", "CHE", "UKR", "GBR", ];
+var oceania = ["ASM", "AUS", "CXR", "CCK", "COK", "FJI", "PYF", "GUM", "HMD", "KIR", "MHL", "FSM", "NRU", "NCL", "NZL", "NIU", "NFK", "MNP", "PLW", "PNG", "PCN", "WSM", "SLB", "TKL", "TON", "TUV", "UMI", "VUT", "WLF"];
+var africa = ["DZA", "AGO", "BEN", "BWA", "IOT", "BFA", "BDI", "CPV", "CMR", "CAF", "TCD", "COM", "COG", "COD", "CIV", "DJI", "EGY", "GNQ", "ERI", "ETH", "ATF", "GAB", "GMB", "GHA", "GIN", "GNB", "KEN", "LSO", "LBR", "LBY", "MDG", "MWI", "MLI", "MRT", "MUS", "MYT", "MAR", "MOZ", "NAM", "NER", "NGA", "REU", "RWA", "SHN", "STP", "SEN", "SYC", "SLE", "SOM", "ZAF", "SSD", "SDN", "SWZ", "TZA", "TGO", "TUN", "UGA", "ESH", "ZMB", "ZWE"];
+
+function checkCounties(region) {
+    var countries = [];
+    switch (region) {
+        case 'asia':
+            countries = asia;
+            break;
+        case 'americas':
+            countries = americas;
+            break;
+        case 'na':
+            countries = na;
+            break;
+        case 'europe':
+            countries = europe;
+            break;
+        case 'oceania':
+            countries = oceania;
+            break;
+        case 'africa':
+            countries = africa;
+            break;
+        default:
+            console.error('Wrong region', region);
+            break;
     }
-    for(i=0;i<EU.length;i++) {
-        $("input[value='"+EU[i]+"']").attr('checked','checked');
+    for (i = 0; i < countries.length; i++) {
+        $("input[value='" + countries[i] + "']").attr('checked', 'checked');
     }
 }
 
 function clearValues() {
-    $("input:checked").each(function (index,element) {
+    $("input:checked").each(function (index, element) {
         e = $(element).val();
         $("input[name^='price_" + e + "']").val('');
         $("input[name^='from_" + e + "']").val('');
@@ -18,84 +44,84 @@ function clearValues() {
 }
 
 function checkOutsideEU() {
-    if(document.location.href.indexOf("twoLetterCodes")>0) {
-        var EU = new Array("BE","BG","CY","DK","DE","EE","FI","FR","GR","HU","HR","IR","IT","LV","LT","LU","MT","NL","AT","PL","PT","RO","SK","SI","ES","CZ","GB","SE");
-    } else {
-        var EU = new Array("BEL","BGR","CYP","DNK","DEU","EST","FIN","FRA","GRC","HUN","HRV","IRN","ITA","LVA","LTU","LUX","MLT","NLD","AUT","POL","PRT","ROU","SVK","SVN","ESP","CZE","GBR","SWE");
-    }
+    // if(document.location.href.indexOf("twoLetterCodes")>0) {
+    var EU = new Array("BE", "BG", "CY", "DK", "DE", "EE", "FI", "FR", "GR", "HU", "HR", "IR", "IT", "LV", "LT", "LU", "MT", "NL", "AT", "PL", "PT", "RO", "SK", "SI", "ES", "CZ", "GB", "SE");
+    // } else {
+    //     var EU = new Array("BEL","BGR","CYP","DNK","DEU","EST","FIN","FRA","GRC","HUN","HRV","IRN","ITA","LVA","LTU","LUX","MLT","NLD","AUT","POL","PRT","ROU","SVK","SVN","ESP","CZE","GBR","SWE");
+    // }
     /*for(i=0;i<EU.length;i++) {
         $("input[value='"+EU[i]+"']").attr('checked','checked');
     }*/
-    $("input[name^='country_']").each(function (index,element) {
-        if(EU.indexOf($(element).val()) === -1) {
-            $(element).attr('checked','checked');
+    $("input[name^='country_']").each(function (index, element) {
+        if (EU.indexOf($(element).val()) === -1) {
+            $(element).attr('checked', 'checked');
         }
     });
 }
 
 function checkOutsideEuro() {
-    if(document.location.href.indexOf("twoLetterCodes")>0) {
-        var EU = new Array("AL","AD","AM","AT","AZ","BY","BE","BA","BG","HR","CY","CZ","DK","EE","FI","FR","GE","DE","GR","HU","IS","IE","IT","KZ","LV","LI","LT","LU","MK","MT","MD","MC","ME","NL","NO","PL","PT","RO","RU","SM","RS","SK","SI","ES","SE","CH","TR","UA","GB","VA");
-    } else {
-        var EU = new Array("ALB","AND","ARM","AUT","AZE","BLR","BEL","BIH","BGR","HRV","CYP","CZE","DNK","EST","FIN","FRA","GEO","DEU","GRC","HUN","ISL","IRL","ITA","KAZ","LVA","LIE","LTU","LUX","MKD","MLT","MDA","MCO","MNE","NLD","NOR","POL","PRT","ROU","RUS","SMR","SRB","SVK","SVN","ESP","SWE","CHE","TUR","UKR","GBR","VAT");
-    }
+    // if(document.location.href.indexOf("twoLetterCodes")>0) {
+    var EU = new Array("AL", "AD", "AM", "AT", "AZ", "BY", "BE", "BA", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "GE", "DE", "GR", "HU", "IS", "IE", "IT", "KZ", "LV", "LI", "LT", "LU", "MK", "MT", "MD", "MC", "ME", "NL", "NO", "PL", "PT", "RO", "RU", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "TR", "UA", "GB", "VA");
+    // } else {
+    //     var EU = new Array("ALB","AND","ARM","AUT","AZE","BLR","BEL","BIH","BGR","HRV","CYP","CZE","DNK","EST","FIN","FRA","GEO","DEU","GRC","HUN","ISL","IRL","ITA","KAZ","LVA","LIE","LTU","LUX","MKD","MLT","MDA","MCO","MNE","NLD","NOR","POL","PRT","ROU","RUS","SMR","SRB","SVK","SVN","ESP","SWE","CHE","TUR","UKR","GBR","VAT");
+    // }
     /*for(i=0;i<EU.length;i++) {
         $("input[value='"+EU[i]+"']").attr('checked','checked');
     }*/
-    $("input[name^='country_']").each(function (index,element) {
-        if(EU.indexOf($(element).val()) === -1) {
-            $(element).attr('checked','checked');
+    $("input[name^='country_']").each(function (index, element) {
+        if (EU.indexOf($(element).val()) === -1) {
+            $(element).attr('checked', 'checked');
         }
     });
 }
 
 function reset() {
-    $("input[name^='country_']").each(function (index,element) {
+    $("input[name^='country_']").each(function (index, element) {
         $(element).removeAttr('checked');
     });
 }
 
 function checkEuro() {
-    if(document.location.href.indexOf("twoLetterCodes")>0) {
-        var EU = new Array("AL","AD","AM","AT","AZ","BY","BE","BA","BG","HR","CY","CZ","DK","EE","FI","FR","GE","DE","GR","HU","IS","IE","IT","KZ","LV","LI","LT","LU","MK","MT","MD","MC","ME","NL","NO","PL","PT","RO","RU","SM","RS","SK","SI","ES","SE","CH","TR","UA","GB","VA");
-    } else {
-        var EU = new Array("ALB","AND","ARM","AUT","AZE","BLR","BEL","BIH","BGR","HRV","CYP","CZE","DNK","EST","FIN","FRA","GEO","DEU","GRC","HUN","ISL","IRL","ITA","KAZ","LVA","LIE","LTU","LUX","MKD","MLT","MDA","MCO","MNE","NLD","NOR","POL","PRT","ROU","RUS","SMR","SRB","SVK","SVN","ESP","SWE","CHE","TUR","UKR","GBR","VAT");
-    }
-    for(i=0;i<EU.length;i++) {
-        $("input[value='"+EU[i]+"']").attr('checked','checked');
+    // if(document.location.href.indexOf("twoLetterCodes")>0) {
+    var EU = new Array("AL", "AD", "AM", "AT", "AZ", "BY", "BE", "BA", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "GE", "DE", "GR", "HU", "IS", "IE", "IT", "KZ", "LV", "LI", "LT", "LU", "MK", "MT", "MD", "MC", "ME", "NL", "NO", "PL", "PT", "RO", "RU", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "TR", "UA", "GB", "VA");
+    // } else {
+    //     var EU = new Array("ALB","AND","ARM","AUT","AZE","BLR","BEL","BIH","BGR","HRV","CYP","CZE","DNK","EST","FIN","FRA","GEO","DEU","GRC","HUN","ISL","IRL","ITA","KAZ","LVA","LIE","LTU","LUX","MKD","MLT","MDA","MCO","MNE","NLD","NOR","POL","PRT","ROU","RUS","SMR","SRB","SVK","SVN","ESP","SWE","CHE","TUR","UKR","GBR","VAT");
+    // }
+    for (i = 0; i < EU.length; i++) {
+        $("input[value='" + EU[i] + "']").attr('checked', 'checked');
     }
 }
 
 function setDefPrice() {
-    $("input:checked").each(function (index,elmnt) {
+    $("input:checked").each(function (index, elmnt) {
         $(elmnt).parent().parent().find("input[name*=price]").val($("#defPrice").val());
     });
 }
 
 function setdefSubtotal() {
-    $("input:checked").each(function (index,elmnt) {
+    $("input:checked").each(function (index, elmnt) {
         $(elmnt).parent().parent().find("input[name*=from]").val($("#defSubtotal").val());
     });
 }
 
 $(window).ready(function () {
-    $("a.addRow").live('click',function () {
+    $("a.addRow").live('click', function () {
         row = $(this).parent().parent().parent().parent().parent().html();
-        row = row.replace("<a href=\"javascript:void()\" class=\"addRow\">+ Add row</a>","");
+        row = row.replace("<a href=\"javascript:void()\" class=\"addRow\">+ Add row</a>", "");
         $(this).parent().parent().parent().parent().parent().after(row);
     });
-    $("label").live('click',function(){
+    $("label").live('click', function () {
         checked = $(this).parent().find('input').attr('checked');
-        if(checked=='checked') {
+        if (checked == 'checked') {
             $(this).parent().find('input').removeAttr('checked');
         } else {
-            $(this).parent().find('input').attr('checked','checked');
+            $(this).parent().find('input').attr('checked', 'checked');
         }
     });
 });
 
 function addRowSelectedCountries() {
-    $("input:checked").each(function (index,element) {
+    $("input:checked").each(function (index, element) {
         li = $(element).closest('li');
         li.find('a.addRow').click();
     });
@@ -103,13 +129,13 @@ function addRowSelectedCountries() {
 
 function changeLabel() {
     var condition = $("#carriers_tablerate_condition_name").val();
-    if(condition=="weight") {
+    if (condition == "weight") {
         var label = "From weight";
         $(".euroCondition").hide();
-    } else if(condition=="price") {
+    } else if (condition == "price") {
         var label = "From subtotal";
         $(".euroCondition").show();
-    } else if(condition=="qty") {
+    } else if (condition == "qty") {
         var label = "From quantity";
         $(".euroCondition").hide();
     }
